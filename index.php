1540abcf718fe470c8fcc1b3ae35cc7136651bbf
panjun.liu <panjun.liu@verycdn.com.cn>
2013-12-13 11:29:32 +0800

    initial project

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * SmartyPHP v1.0
+ */
+
+//Start the Session
+session_start(); 
+
+// Defines
+define('ROOT_DIR', realpath(dirname(__FILE__)) .'/');
+define('APP_DIR', ROOT_DIR .'application/');
+
+// Includes
+require(APP_DIR .'config/config.php');
+require(ROOT_DIR .'system/model.php');
+require(ROOT_DIR .'system/view.php');
+require(ROOT_DIR .'system/controller.php');
+require(ROOT_DIR .'system/SmartyPHP.php');
+
+// Define base URL
+global $config;
+define('BASE_URL', $config['base_url']);
+
+SmartyPHP();
+
+?>
